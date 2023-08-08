45218bf9cf2ab2f7f8eedc9dbbc814dbbb8ff281
VorobievKA <vorobievka@gmail.com>
2023-08-08 20:20:42 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ints calc = new IntsCalculator();
+
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+
+    }
+}
